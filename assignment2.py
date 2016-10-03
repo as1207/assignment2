2e2b5dde581899a2b147d626bd3cd918ca58e8a6
as1207 <asydno1@students.towson.edu>
2016-10-03 16:34:37 -0400

    Update and rename passcrack.py to assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,39 @@
+import os,sys,crypt
+os.system('clear')
+
+def findPass(cryptPass,user):
+	dictionFile = open ('diction.txt','r')
+	ctype = cryptPass.split("$")[1]
+	found = False
+	salt = cryptPass.split ("$")[2]
+	insalt= "$" + ctype + "$" + salt + "$"
+	for word in dictionFile.readlines():
+		word= word.strip('\n')
+		cryptWord = crypt.crypt(word,insalt)
+		if( cryptWord == cryptPass):
+			found= True
+				
+			print "[*]YAY! Found password for: " + user + " || and the password is... " + word
+			
+	if ( found == False): 
+		print "[*]DARN! Password not found in dictionary."
+
+	print "--------------------------------------------------------------------"
+			
+			
+exit	
+
+def main(): 
+	found = False
+	passFile = open ('/etc/shadow','r')
+	for line in passFile.readlines():
+		line = line.replace("\n","").split(":")
+		if not line[1] in ['x', '*', '!']:
+			user = line[0]
+			cryptPass = line[1]
+			print "Lets crack the password for: " + user
+			findPass(cryptPass,user)
+		if(found == None or user == None):
+				print "[*] User does not exist or user does not have a password."
+if __name__=="__main__" :
+	main()
